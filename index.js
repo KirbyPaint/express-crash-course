f22f21fde9b5e260f55e5f7e6f948f0ef09681e8
Ash Porter <ashley.porter@eyecuelab.com>
2021-09-30 12:38:55 -0700

    add handlebars pages

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require("express");
 const path = require("path");
 // const logger = require("./middleware/logger");
+const exphbs = require("express-handlebars");
 
 const app = express();
 
 // Initialize middleware
 // app.use(logger);
 
+// Handlebars Middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Set static folder
+// Homepage Route
+app.get("/", (req, res) => res.render("index"));
+
+// Set static folder - since this comes after homepage,
+// it no longer renders
+// Usually there aren't both static folders and handlebars things
 app.use(express.static(path.join(__dirname, "public")));
 
 // Members API routes
