ddd788a8a6e77f93250aacdba28268db1c58566e
Ash Porter <ashley.porter@eyecuelab.com>
2021-09-30 11:58:58 -0700

    add error handling

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const path = require("path");
 const members = require("./Members");
-const logger = require("./middleware/logger");
+// const logger = require("./middleware/logger");
 
 const app = express();
 
 // Initialize middleware
 // app.use(logger);
 
+// Get all members
 app.get("/api/members", (req, res) => {
   res.json(members);
+});
+
+app.get("/api/members/:id", (req, res) => {
+  const found = members.some((member) => member.id === req.params.id);
+
+  if (found) {
+    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
 });
 
 // Set static folder
