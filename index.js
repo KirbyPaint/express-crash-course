2093cfa4ae7d8b30b6761e29db60371f96674cfd
Ash Porter <ashley.porter@eyecuelab.com>
2021-09-30 11:36:20 -0700

    move middleware into its own folder

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const path = require("path");
 const members = require("./Members");
+const logger = require("./middleware/logger");
 
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log(`${req.protocol}://${req.get("host")}${req.originalUrl}`);
-  next();
-};
-
 // Initialize middleware
-app.use(logger);
+// app.use(logger);
 
 app.get("/api/members", (req, res) => {
   res.json(members);
