cb2da929e12cf71325850a77b9274490801fe2eb
Ash Porter <ashley.porter@eyecuelab.com>
2021-09-30 11:26:50 -0700

    add get route for basic json

--- index.js
+++ index.js
@@ -3,6 +3,31 @@ const path = require("path");
 
 const app = express();
 
+const members = [
+  {
+    id: 1,
+    name: "Samus",
+    email: "samus@samus.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "Kirby",
+    email: "kirby@kirby.com",
+    status: "active",
+  },
+  {
+    id: 3,
+    name: "Fox",
+    email: "fox@fox.com",
+    status: "inactive",
+  },
+];
+
+app.get("/api/members", (req, res) => {
+  res.json(members);
+});
+
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
